40abfc4398af0b8a48ba11d2c651821626487c51
stephen911 <stephendappah1@gmail.com>
2022-03-23 22:45:44 +0000

    'new'

--- new.py
+++ new.py
@@ -18,4 +18,6 @@ class Model(object):
                     1.  Set the model instance variable to the corresponding model class which
                             which will provide methods `fit` and `predict`.
                             
-                            
+                                2.  Should implement the following abstract methods `load_model`,
+                                        `save_model` `train` and `evaluate`. These methods provide the
+                                        
