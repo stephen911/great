1df46e6979bc855c23e9063b58874cd0f531f4f3
stephen911 <stephendappah1@gmail.com>
2022-03-23 23:00:53 +0000

    'new'

--- new.py
+++ new.py
@@ -25,4 +25,5 @@ class Model(object):
                                                                     
                                                                         Attributes:
                                                                                 model (Any): instance variable that holds the model.
-                                                                                
+                                                                                        save_path (str): path to save the model.
+                                                                                        
