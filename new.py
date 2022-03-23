2ba505a843865f94a7a6ce1fee6b419294a566f7
stephen911 <stephendappah1@gmail.com>
2022-03-23 22:45:20 +0000

    'new'

---
+++ new.py
@@ -0,0 +1,17 @@
+"""
+speechemotionrecognition module.
+Provides a library to perform speech emotion recognition on `emodb` data set
+"""
+import sys
+from typing import Tuple
+
+import numpy
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+
+
+class Model(object):
+        """
+            Model is the abstract class which determines how a model should be.
+                Any model inheriting this class should do the following.
+                
