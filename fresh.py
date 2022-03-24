b6c9ba510bc43b835148b49371267d4f20049bf6
stephen911 <stephendappah1@gmail.com>
2022-03-24 01:17:40 +0000

    'new'

--- fresh.py
+++ fresh.py
@@ -153,4 +153,5 @@
                                     ~Import dependencies:
                                         import pygame ~Game library
                                         from pygame.locals import * ~For useful variables
+                                        import copy ~Library used to make exact copies of lists.
                                         )
