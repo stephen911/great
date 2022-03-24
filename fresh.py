6d94ed03374a40f4b9d2fc27bbbb2c282dd879da
stephen911 <stephendappah1@gmail.com>
2022-03-24 01:20:10 +0000

    'new'

--- fresh.py
+++ fresh.py
@@ -171,4 +171,6 @@
                                                 ~ castling rights of the players, number of irreversible moves played so far, the positions of
                                                 ~ pieces on the board, etc.
                                                 ~ 2. Shades - This is used for GUI. A shade is a transparent colored image that is displayed on
+                                                ~ a specific square of the chess board, in order to show various things to the user such as
+                                                ~ the squares to which a piece may move, the square that is currently selected, etc. The class
                                                 )
