5007e4bf2f540d7884288cc2436f3a5083f26c6e
stephen911 <stephendappah1@gmail.com>
2022-03-24 01:20:18 +0000

    'new'

--- fresh.py
+++ fresh.py
@@ -173,4 +173,6 @@
                                                 ~ 2. Shades - This is used for GUI. A shade is a transparent colored image that is displayed on
                                                 ~ a specific square of the chess board, in order to show various things to the user such as
                                                 ~ the squares to which a piece may move, the square that is currently selected, etc. The class
+                                                ~ stores a reference to the image that the instance of the class should display when needed. It
+                                                ~ also stores the coordinates at which the shade would be applied.
                                                 )
