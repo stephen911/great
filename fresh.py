fb52e3d5030725c74b662bcd1ba92e48699f3913
stephen911 <stephendappah1@gmail.com>
2022-03-24 00:37:10 +0000

    'new'

--- fresh.py
+++ fresh.py
@@ -250,4 +250,5 @@
                                                                     import pygame ~Game library
                                                                     from pygame.locals import * ~For useful variables
                                                                     import copy ~Library used to make exact copies of lists.
+                                                                    import pickle ~Library used to store dictionaries in a text file and read them from text files.
                                                                     
