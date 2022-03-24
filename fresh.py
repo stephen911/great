3932e9b6d6be7b3059c2331da2719d165fd08183
stephen911 <stephendappah1@gmail.com>
2022-03-24 00:39:42 +0000

    'new'

--- fresh.py
+++ fresh.py
@@ -266,4 +266,9 @@
                                                                             ~ features that specify the state of the game, such as the the player that has to play next,
                                                                             ~ castling rights of the players, number of irreversible moves played so far, the positions of
                                                                             ~ pieces on the board, etc.
+                                                                            ~ 2. Shades - This is used for GUI. A shade is a transparent colored image that is displayed on
+                                                                            ~ a specific square of the chess board, in order to show various things to the user such as
+                                                                            ~ the squares to which a piece may move, the square that is currently selected, etc. The class
+                                                                            ~ stores a reference to the image that the instance of the class should display when needed. It
+                                                                            ~ also stores the coordinates at which the shade would be applied.
                                                                             
