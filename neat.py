051699a55a19a833b92a48a07b65585b61552c74
stephen911 <stephendappah1@gmail.com>
2022-03-24 02:19:04 +0000

    'new'

--- neat.py
+++ neat.py
@@ -2,6 +2,8 @@
 ~    15-112: Principles of Programming and Computer Science
 ~    Project: Chess Program
 ~    Name      : Muhammad Nahin Khan
+~    AndrewID  : mnk1
+~    File Created: 07/11/2016
 
 
 """
