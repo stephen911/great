2c6c4fca8f9c7a64b0dc2baa0c325e428c8d13a6
stephen911 <stephendappah1@gmail.com>
2022-03-24 00:21:51 +0000

    'new'

--- fresh.py
+++ fresh.py
@@ -128,4 +128,5 @@
                                 ~    27/11 02:24       27/11 03:08
                                 ~
                                 
+                                ~Ensure that Pygame is installed
                                 
