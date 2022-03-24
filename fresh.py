8e7ab4e2f4e419d279f5be55633bf5879e48da78
stephen911 <stephendappah1@gmail.com>
2022-03-24 00:17:56 +0000

    'new'

--- fresh.py
+++ fresh.py
@@ -96,4 +96,5 @@
                             ~        queen, it has an advantage over black.
                             ~    15-112: Principles of Programming and Computer Science
                             ~    Project: Chess Program
+                            ~    Name      : Muhammad Nahin Khan
                             
