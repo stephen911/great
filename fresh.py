42f6d5c794717f60c6d0084ef1797c7f67d658d0
stephen911 <stephendappah1@gmail.com>
2022-03-24 00:30:05 +0000

    'new'

--- fresh.py
+++ fresh.py
@@ -193,4 +193,6 @@
                                                         ~    b) Piece-square table values - for each piece, there is a table that stores the best
                                                         ~        squares that the particular piece should occupy. So if white has a knight at a
                                                         ~        good square that controls the centre of the board, whereas black has a knight
+                                                        ~        at the corner of the board, the situation is evaluated as being more favourable
+                                                        ~        for white.
                                                         
