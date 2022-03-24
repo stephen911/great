d8a206f5bc1a0481130d71ae050b50538e73f079
stephen911 <stephendappah1@gmail.com>
2022-03-24 00:25:25 +0000

    'new'

--- fresh.py
+++ fresh.py
@@ -156,4 +156,6 @@
                                                 ~ drag and drop movement of pieces as well as click-click movement.
                                                 ~
                                                 ~ The AI that plays against the human evaluates all possible moves made by either
+                                                ~ player up to a certain level of depth. The AI evaluates each position by giving
+                                                ~ it a score. The higher the value of the score, the more favourable a position
                                                 
