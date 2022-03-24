c330fbaa277b489e2a4ec3b4fc0080d5535ef1e7
stephen911 <stephendappah1@gmail.com>
2022-03-24 00:42:12 +0000

    'new'

--- fresh.py
+++ fresh.py
@@ -290,4 +290,5 @@
                                                                                                                                                                                 ~these keys represents the number of times each of these positions was repeated in order for this
                                                                                                                                                                                         ~position to be reached.
                                                                                                                                                                                                 
-                                                                                                                                                                                                
+                                                                                                                                                                                                    def getboard(self):
+                                                                                                                                                                                                        
