7826c13416591a71ba652e59dd1826d054defeb2
stephen911 <stephendappah1@gmail.com>
2022-03-24 00:24:48 +0000

    'new'

--- fresh.py
+++ fresh.py
@@ -152,4 +152,5 @@
                                                 ~ The game state is mainly stored as a 2D list of strings, and most of the
                                                 ~ processing is thus done on a list of strings.
                                                 ~
+                                                ~ The GUI takes the current state and displays it on the screen. The GUI allows
                                                 
