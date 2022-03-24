21bdd10c892e92d8bfa5e1554ca5c2636a4f5ba5
stephen911 <stephendappah1@gmail.com>
2022-03-24 01:20:48 +0000

    'new'

--- fresh.py
+++ fresh.py
@@ -178,4 +178,5 @@
                                                 ~ 3. Piece - This is also used for GUI. A Piece object stores the information about the image
                                                 ~ that a piece should display (pawn, queen, etc.) and the coordinate at which it should be
                                                 ~ displayed on thee chess board.
+                                                ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
                                                 )
